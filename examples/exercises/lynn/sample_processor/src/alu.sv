232b349a66c59d89c7a61b62b625ab602763a222
Huanhuan Huang <huahuang@g.hmc.edu>
2026-04-26 23:35:26 -0700

    reduced shifters in alu

--- examples/exercises/lynn/sample_processor/src/alu.sv
+++ examples/exercises/lynn/sample_processor/src/alu.sv
@@ -8,7 +8,7 @@ module alu(
 );
 
     logic [31:0] CondInvb, Sum, SLT, CondInvb2;
-    logic [31:0] AddSum;
+    logic [31:0] AddSum, revSrcA, shiftSrcA, revALUResult, ALUFinal;
     logic       ALUOp, Sub, Overflow, Neg, LT;
     logic [2:0]  ALUFunct;
     logic [63:0] midALUResult;
@@ -20,11 +20,11 @@ module alu(
     assign AddSum = SrcA + SrcB;
     assign IEUAdr = AddSum;
 
-    // Add/sub path for ALUResult when needed
+    // Add/sub path for ALUFinal when needed
     assign CondInvb = Sub ? ~SrcB : SrcB;
     assign CondInvb2 = Sub ? (~SrcB + 32'b1) : SrcB;
     assign Sum      = SrcA + CondInvb2;
-
+    reversal #(1) revA(SrcA, revSrcA);
     // SLT (signed) based on subtraction result
     assign Overflow = Sub &
                   (SrcA[31] ^ SrcB[31]) &
@@ -33,48 +33,77 @@ module alu(
     assign LT       = Neg ^ Overflow;
     assign SLT      = {31'b0, LT};
 
+    assign shiftSrcA = (Funct3==3'b001) ? revSrcA: SrcA;
+
     assign ALUFunct = (Funct3 & {3{ALUOp}}); // ^ {1'b0, ~Funct7E[4] & IsZbbE & ALUOp & ~Funct3[0], 1'b0};
     //assign SrcA64 = {32'b0, SrcA};
     always_comb begin
         case (ALUFunct)
-            3'b000: midALUResult = {32'b0, Sum};
-            3'b010: midALUResult = {32'b0, SLT};
-            3'b100: midALUResult = {32'b0, SrcA ^ CondInvb};
-            3'b110: midALUResult = {32'b0, SrcA | CondInvb};
-            3'b111: midALUResult = {32'b0, SrcA & CondInvb};
-            3'b001: midALUResult = {32'b0, SrcA} << SrcB[4:0];
-            3'b011: midALUResult = {63'b0, (SrcA < SrcB)};  // SLTU (unsigned)
-            3'b101: begin
+            3'b000: begin
+                        ALUFinal = Sum;
+                        midALUResult = 64'b0;
+                    end
+            3'b010: begin
+                        ALUFinal = SLT;
+                        midALUResult = 64'b0;
+                    end
+            3'b100: begin
+                        ALUFinal = SrcA ^ CondInvb;
+                        midALUResult = 64'b0;
+                    end
+            3'b110: begin
+                        ALUFinal = SrcA | CondInvb;
+                        midALUResult = 64'b0;
+                    end
+            3'b111: begin
+                        ALUFinal =  SrcA & CondInvb;
+                        midALUResult = 64'b0;
+                    end
+            3'b011: begin
+                        ALUFinal = {31'b0, (SrcA < SrcB)};  // SLTU (unsigned)
+                        midALUResult = 64'b0;
+                    end
+            3'b001, 3'b101: begin
                 if (Funct7E[5] & ~Funct7E[4]) begin
                     // SRA: manual sign-fill (no reliance on >>>)
-                    if (SrcB[4:0] == 0)
-                        midALUResult = {32'b0, SrcA};
-                    else
-                        midALUResult = {32'b0, $signed(SrcA) >>> SrcB[4:0]};
+                    if (SrcB[4:0] == 0) begin
+                        ALUFinal = shiftSrcA;
+                        midALUResult = 64'b0;
+                    end else begin
+                        midALUResult = {32'b0, $signed(shiftSrcA) >>> SrcB[4:0]};
+                        ALUFinal = (IsZbbE) ? midALUResult[63:32] | midALUResult[31:0] : midALUResult[31:0];
+                        end
                     end else begin
                         // SRL
-                        midALUResult = {SrcA, 32'b0} >> SrcB[4:0];
+                        midALUResult = {shiftSrcA, 32'b0} >> SrcB[4:0];
+                        ALUFinal = (IsZbbE) ? midALUResult[63:32] | midALUResult[31:0] : midALUResult[63:32];
                     end
                 end
-            default: midALUResult = '0; // (use 0 instead of 'x to avoid X-prop)
+            default: begin
+                        ALUFinal = '0; // (use 0 instead of 'x to avoid X-prop)
+                        midALUResult = 64'b0;
+                    end
         endcase
     end
 
-    // assign ALUResult = IsZbbE ? midALUResult[63:32] | midALUResult[31:0]
+    reversal #(1) revRes(ALUFinal, revALUResult);
+
+    assign ALUResult = (ALUFunct==3'b001) ? revALUResult : ALUFinal;
+    // assign ALUFinal = IsZbbE ? midALUResult[63:32] | midALUResult[31:0]
     //                     : ((Funct7E==7'b0) & (Funct3==3'b101) ? midALUResult[63:32] : midALUResult[31:0]); //choose top only if srl
 
 
-    assign isShiftRight =
+    /* assign isShiftRight =
         ALUOp &&
         !IsZbbE &&
         !IsZbaE &&
         (Funct3 == 3'b101) &&
         !(Funct7E[5] & ~Funct7E[4]); // only SRL/SRLI uses upper half
 
-    assign ALUResult =
+    assign ALUFinal =
         IsZbbE ? (midALUResult[63:32] | midALUResult[31:0]) :
         isShiftRight ? midALUResult[63:32] :
-        midALUResult[31:0];
+        midALUResult[31:0]; */
 
-    //assign ALUResult = (Funct7E==7'b0 & Funct3==3'b101 ? midALUResult[63:32] : midALUResult[31:0]); //choose top only if srl
+    //assign ALUFinal = (Funct7E==7'b0 & Funct3==3'b101 ? midALUResult[63:32] : midALUResult[31:0]); //choose top only if srl
 endmodule
